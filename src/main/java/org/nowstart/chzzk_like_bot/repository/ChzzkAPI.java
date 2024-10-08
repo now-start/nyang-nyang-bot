2aa3b98744eadf1660c3b870f7e1825ccbb210f3
moon <nowstart2691@gmail.com>
2024-10-08 20:49:37 +0900

    feat: ID 값은 조회가 안되네... 핫픽스 처리

--- src/main/java/org/nowstart/chzzk_like_bot/repository/ChzzkAPI.java
+++ src/main/java/org/nowstart/chzzk_like_bot/repository/ChzzkAPI.java
@@ -5,6 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@Deprecated
 @FeignClient(name = "chzzk-api", url = "https://api.chzzk.naver.com")
 public interface ChzzkAPI {
     @GetMapping("/service/v1/search/channels")
