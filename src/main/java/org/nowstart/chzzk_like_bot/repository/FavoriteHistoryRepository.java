3c56ebece5f5c86ea10c59b35bf9190c30e08545
moon <nowstart2691@gmail.com>
2024-10-08 22:13:42 +0900

    feat: 와 히스토리 조회도 끝 근데 디자인 망함

--- src/main/java/org/nowstart/chzzk_like_bot/repository/FavoriteHistoryRepository.java
+++ src/main/java/org/nowstart/chzzk_like_bot/repository/FavoriteHistoryRepository.java
@@ -1,12 +1,13 @@
 // FavoriteHistoryRepository.java
 package org.nowstart.chzzk_like_bot.repository;
 
-import java.util.List;
 import org.nowstart.chzzk_like_bot.entity.FavoriteHistoryEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FavoriteHistoryRepository extends JpaRepository<FavoriteHistoryEntity, Long> {
-    List<FavoriteHistoryEntity> findByFavoriteEntityUserId(String userId);
+    Page<FavoriteHistoryEntity> findByFavoriteEntityUserId(Pageable pageable, String userId);
 }
