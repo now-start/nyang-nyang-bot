734fa6ccfdec9e9448f5adaad4895c7d63aee68f
moon <nowstart2691@gmail.com>
2024-10-13 01:26:21 +0900

    feat: command 로그 추가

--- src/main/java/org/nowstart/chzzk_like_bot/config/ChzzkChatListenerConfig.java
+++ src/main/java/org/nowstart/chzzk_like_bot/config/ChzzkChatListenerConfig.java
@@ -2,12 +2,14 @@ package org.nowstart.chzzk_like_bot.config;
 
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.nowstart.chzzk_like_bot.command.Command;
 import org.springframework.stereotype.Component;
 import xyz.r2turntrue.chzzk4j.chat.ChatEventListener;
 import xyz.r2turntrue.chzzk4j.chat.ChatMessage;
 import xyz.r2turntrue.chzzk4j.chat.ChzzkChat;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class ChzzkChatListenerConfig implements ChatEventListener {
@@ -24,6 +26,7 @@ public class ChzzkChatListenerConfig implements ChatEventListener {
     public void onChat(ChatMessage msg) {
         Command command = commands.get(msg.getContent().split(" ")[0]);
         if (command != null) {
+            log.info("[Commend][{}]", msg);
             command.execute(chat, msg);
         }
     }
