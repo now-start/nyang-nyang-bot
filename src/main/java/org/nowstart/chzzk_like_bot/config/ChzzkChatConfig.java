48c9b56fb1ea26039609a3376a9b24ad836a14ce
moon <nowstart2691@gmail.com>
2024-10-12 23:33:20 +0900

    feat: 방송 확인 후 채팅창 연결

--- src/main/java/org/nowstart/chzzk_like_bot/config/ChzzkChatConfig.java
+++ src/main/java/org/nowstart/chzzk_like_bot/config/ChzzkChatConfig.java
@@ -1,14 +1,18 @@
 package org.nowstart.chzzk_like_bot.config;
 
 import jakarta.annotation.PostConstruct;
-import java.io.IOException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import xyz.r2turntrue.chzzk4j.Chzzk;
 import xyz.r2turntrue.chzzk4j.ChzzkBuilder;
 import xyz.r2turntrue.chzzk4j.chat.ChzzkChat;
 
+@Slf4j
 @Configuration
 @RequiredArgsConstructor
 public class ChzzkChatConfig {
@@ -20,16 +24,33 @@ public class ChzzkChatConfig {
     @Value("${chzzk.ses}")
     private String ses;
     private final ChzzkChatListenerConfig chzzkChatListenerConfig;
+    private final ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
+    private ChzzkChat activeChat;
 
     @PostConstruct
-    //TODO: aut, ses 값을 지속적으로 들고올 방법을 찾아야 됨
-    public void startChat() throws IOException {
+    public void startChat() {
         Chzzk chzzk = new ChzzkBuilder()
             .withAuthorization(aut, ses)
             .build();
-        ChzzkChat chat = chzzk.chat(channelId)
-            .withChatListener(chzzkChatListenerConfig)
-            .build();
-        chat.connectAsync();
+
+        executor.scheduleAtFixedRate(() -> {
+            try {
+                boolean currentBroadcastingStatus = chzzk.getChannel(channelId).isBroadcasting();
+                if (currentBroadcastingStatus && activeChat == null) {
+                    log.info("====================[START][CHAT]====================");
+                    activeChat = chzzk.chat(channelId)
+                        .withChatListener(chzzkChatListenerConfig)
+                        .withAutoReconnect(false)
+                        .build();
+                    activeChat.connectBlocking();
+                } else if (!currentBroadcastingStatus && activeChat != null) {
+                    log.info("====================[END][CHAT]====================");
+                    activeChat.closeBlocking();
+                    activeChat = null;
+                }
+            } catch (Exception e) {
+                log.error("====================[ERROR][CHAT]====================", e);
+            }
+        }, 0, 1, TimeUnit.SECONDS);
     }
 }
