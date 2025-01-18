86c9085c39ee6c0228e07a9c635e8755a690e879
moon <nowstart2691@gmail.com>
2025-01-18 22:30:40 +0900

    feat: 어드민 통합

--- src/main/java/org/nowstart/nyangnyangbot/NyangNyangBotApplication.java
+++ src/main/java/org/nowstart/nyangnyangbot/NyangNyangBotApplication.java
@@ -1,6 +1,5 @@
 package org.nowstart.nyangnyangbot;
 
-import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -10,7 +9,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @EnableJpaAuditing
-@EnableAdminServer
 @EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
