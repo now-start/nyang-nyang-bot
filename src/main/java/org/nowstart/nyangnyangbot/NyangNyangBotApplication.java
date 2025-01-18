34e7bfa794ff2173577aeb4cb7fb8fe3d88b1452
moon <nowstart2691@gmail.com>
2025-01-18 21:44:12 +0900

    feat: 유레카 추가

--- src/main/java/org/nowstart/nyangnyangbot/NyangNyangBotApplication.java
+++ src/main/java/org/nowstart/nyangnyangbot/NyangNyangBotApplication.java
@@ -3,6 +3,7 @@ package org.nowstart.nyangnyangbot;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -11,6 +12,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableJpaAuditing
 @EnableAdminServer
 @EnableFeignClients
+@EnableDiscoveryClient
 @SpringBootApplication
 public class NyangNyangBotApplication {
 
