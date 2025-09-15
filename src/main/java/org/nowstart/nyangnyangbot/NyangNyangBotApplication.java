1a00c47e08fbd0a250a2bef9ed9eafde92ade4a6
moon <nowstart2691@gmail.com>
2025-09-15 21:40:46 +0900

    feat: 버스 리프레쉬 추가

--- src/main/java/org/nowstart/nyangnyangbot/NyangNyangBotApplication.java
+++ src/main/java/org/nowstart/nyangnyangbot/NyangNyangBotApplication.java
@@ -2,6 +2,7 @@ package org.nowstart.nyangnyangbot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -10,6 +11,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableJpaAuditing
 @EnableDiscoveryClient
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class NyangNyangBotApplication {
 
     public static void main(String[] args) {
