fd60b481cfef7ff839f253dbf1fbb74e9ed65580
moon <nowstart2691@gmail.com>
2025-09-15 21:40:46 +0900

    feat: 버스 리프레쉬 추가

--- src/main/java/org/nowstart/nyangnyangbot/data/property/ChzzkProperty.java
+++ src/main/java/org/nowstart/nyangnyangbot/data/property/ChzzkProperty.java
@@ -2,10 +2,8 @@ package org.nowstart.nyangnyangbot.data.property;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
 
 @Data
-@Component
 @ConfigurationProperties(prefix = "chzzk")
 public class ChzzkProperty {
 
