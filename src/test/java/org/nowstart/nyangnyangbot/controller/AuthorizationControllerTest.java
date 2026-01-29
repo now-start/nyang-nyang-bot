5405e7e534229fb66fc5e5c2943a7bc8be039fc4
moon <nowstart2691@gmail.com>
2026-01-29 10:36:02 +0900

    ci: update build workflow with reusable workflows and release support

--- src/test/java/org/nowstart/nyangnyangbot/controller/AuthorizationControllerTest.java
+++ src/test/java/org/nowstart/nyangnyangbot/controller/AuthorizationControllerTest.java
@@ -12,7 +12,7 @@ import org.junit.jupiter.api.Test;
 import org.nowstart.nyangnyangbot.data.property.ChzzkProperty;
 import org.nowstart.nyangnyangbot.service.AuthorizationService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
